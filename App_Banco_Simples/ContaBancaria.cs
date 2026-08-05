b5fc0796892ef58bcf327fca3f9eabc5db8b2a1a
Chinnutzz <Chinnutzz@CHINNUTZZ_PC>
2026-08-05 17:58:03 -0300

    Add project files.

---
+++ App_Banco_Simples/ContaBancaria.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App_Banco_Simples
+{
+    internal class ContaBancaria
+    {
+        public string NomeTitular { get; }
+        public int NumeroConta { get; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(string nomeTitular, int numeroConta, double saldoInicial)
+        {
+            NomeTitular = nomeTitular;
+            NumeroConta = numeroConta;
+            Saldo = saldoInicial;
+        }
+
+        public void Depositar(double valor)
+        {
+            if (valor > 0)
+            {
+                Saldo += valor;
+            }
+            else
+            {
+                throw new ArgumentException("O valor do depósito deve ser positivo.");
+            }
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor > 0)
+            {
+                if (valor <= Saldo)
+                {
+                    Saldo -= valor;
+                }
+                else
+                {
+                    throw new InvalidOperationException("Saldo insuficiente para o saque.");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("O valor do saque deve ser positivo.");
+            }
+        }
+    }
+}
---
+++ App_Banco_Simples/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace App_Banco_Simples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao Banco Simples!");
+            // Solicitar informações do usuário
+            Console.Write("Digite o nome do titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+            Console.Write("Digite o número da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o saldo inicial da conta: ");
+            double saldoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            // Criar uma instância da classe ContaBancaria
+            ContaBancaria conta = new ContaBancaria(nomeTitular, numeroConta, saldoInicial);
+            // Exibir informações da conta
+            Console.WriteLine("\nInformações da Conta:");
+            Console.WriteLine($"Titular: {conta.NomeTitular}");
+            Console.WriteLine($"Número da Conta: {conta.NumeroConta}");
+            Console.WriteLine($"Saldo Inicial: {conta.Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+            // Solicitar depósito
+            Console.Write("\nDigite o valor a ser depositado: ");
+            double valorDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Depositar(valorDeposito);
+            Console.WriteLine($"Novo Saldo após depósito: {conta.Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+            // Solicitar saque
+            Console.Write("\nDigite o valor a ser sacado: ");
+            double valorSaque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Sacar(valorSaque);
+            Console.WriteLine($"Novo Saldo após saque: {conta.Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
